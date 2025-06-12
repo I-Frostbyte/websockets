8a33ed6225a2f152111d1f104ceb761c7f6e269d
I-Frostbyte <irefyre@gmail.com>
2025-06-12 10:17:55 +0100

    ClientLists, Egress, Frontend, Backend

---
+++ client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+type ClientList map[*Client]bool
+
+type Client struct {
+	connection *websocket.Conn
+	manager *Manager
+
+	// egress is used to avoid concurrent writes on the websocket connection
+	egress chan []byte
+}
+
+func NewClient(conn *websocket.Conn, manager *Manager) *Client {
+	return &Client{
+		connection: conn,
+		manager: manager,
+		egress: make(chan []byte),
+	}
+}
+
+func (c *Client) readMessages() {
+	defer func() {
+		// cleanup connection
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		messageType, payload, err := c.connection.ReadMessage()
+
+		if err != nil {
+			// CloseGoingAway and CloseAbnormalClosure are checks to ensure the err is not triggered by client or server.
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure){
+				log.Printf("error reading message: %v", err)
+			}
+			break
+		}
+
+		for wsClient := range c.manager.clients {
+			wsClient.egress <- payload
+		}
+
+		log.Println(messageType)
+		log.Println(string(payload))
+	}
+}
+
+// Websocket has a few events
+// ClosedEvent
+// ErrorEvent
+// MessageEvent
+// OpenEvent
+
+func (c *Client) writeMessages() {
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println("connection closed: ", err)
+				}
+				return
+			}
+
+			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Printf("failed to send message: %v", err)
+			}
+			log.Println("message sent")
+		}
+	}
+}
