e3526497cae8c627d169a3e8e6649d8c3f2fee2e
I-Frostbyte <irefyre@gmail.com>
2025-06-12 18:19:54 +0100

    Authentication via OTP

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/I-Frostbyte/websockets
 
 go 1.23.4
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/goccy/go-json v0.10.5
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- manager.go
+++ manager.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
+	"github.com/goccy/go-json"
 	"github.com/gorilla/websocket"
 )
 
@@ -22,13 +25,16 @@ type Manager struct {
 	clients ClientList
 	sync.RWMutex
 
+	otps RetentionMap
+
 	handlers map[string]EventHandler
 }
 
-func NewManager() *Manager {
+func NewManager(ctx context.Context) *Manager {
 	m := &Manager{
 		clients: make(ClientList),
 		handlers: make(map[string]EventHandler),
+		otps: NewRetentionMap(ctx, 5*time.Second),
 	}
 
 	m.setupEventHandlers()
@@ -57,6 +63,18 @@ func (m *Manager) routeEvent(event Event, c *Client) error {
 }
 
 func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
+	// Verifying the otp before connection.
+	otp := r.URL.Query().Get("otp")
+	if otp == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if !m.otps.VerifyOTP(otp) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	log.Println("new connection")
 
 	// upgrade regular http connection into websocket
@@ -73,6 +91,49 @@ func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
 	// Start client processes
 	go client.readMessages()
 	go client.writeMessages()
+}
+
+// loginHandler takes the requests to the login endpoint and processes them.
+// This is where the user is authenticated, the otp is created and the key is sent back.
+// The key in the header shall then serve to grant the user continous access to the websocket.
+func (m *Manager) loginHandler(responseWriter http.ResponseWriter, httpRequest *http.Request) {
+	type userLoginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	var req userLoginRequest
+
+	if err := json.NewDecoder(httpRequest.Body).Decode(&req); err != nil {
+		http.Error(responseWriter, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// hardcoded authentication, can be replaced later with proper db interaction.
+	if req.Username == "gilbert" && req.Password == "insider" {
+		type response struct {
+			OTP string `json:"otp"`
+		}
+
+		otp := m.otps.NewOTP()
+
+		resp := response{
+			OTP: otp.Key,
+		}
+
+		data, err := json.Marshal(resp)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		responseWriter.WriteHeader(http.StatusOK)
+		responseWriter.Write(data)
+		return
+
+	}
+
+	responseWriter.WriteHeader(http.StatusUnauthorized)
 }
 
 func (m *Manager) addClient(client *Client) {
