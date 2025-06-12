6511908e6cc4042bc04528a640e954ecffd558d0
I-Frostbyte <irefyre@gmail.com>
2025-06-12 15:40:01 +0100

    Events, Heartbeating

--- client.go
+++ client.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+var (
+	// pongWait is the duration for how long we will await a pong from a client before dropping the connection.
+	pongWait = 10 * time.Second
+
+	// pingInterval is how often we will send pings to the client. It needs to always be smaller than the pongWait.
+	pingInterval = (pongWait * 9) / 10
 )
 
 type ClientList map[*Client]bool
 
 type Client struct {
 	connection *websocket.Conn
-	manager *Manager
+	manager    *Manager
 
 	// egress is used to avoid concurrent writes on the websocket connection
-	egress chan []byte
+	egress chan Event
 }
 
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
-		manager: manager,
-		egress: make(chan []byte),
+		manager:    manager,
+		egress:     make(chan Event),
 	}
 }
 
@@ -30,23 +40,36 @@ func (c *Client) readMessages() {
 		c.manager.removeClient(c)
 	}()
 
+	// SetReadDeadline from the gorilla package allows us to set a time for how long we should wait
+	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		log.Println(err)
+		return
+	}
+
+	// SetPongHandler applies a certain handler when a pong is triggered.
+	c.connection.SetPongHandler(c.pongHandler)
+
 	for {
-		messageType, payload, err := c.connection.ReadMessage()
+		_, payload, err := c.connection.ReadMessage()
 
 		if err != nil {
 			// CloseGoingAway and CloseAbnormalClosure are checks to ensure the err is not triggered by client or server.
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure){
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error reading message: %v", err)
 			}
 			break
 		}
 
-		for wsClient := range c.manager.clients {
-			wsClient.egress <- payload
+		var request Event
+
+		if err := json.Unmarshal(payload, &request); err != nil {
+			log.Printf("error marshalling event :%v", err)
+			break
 		}
 
-		log.Println(messageType)
-		log.Println(string(payload))
+		if err := c.manager.routeEvent(request, c); err != nil {
+			log.Println("error handling message: ", err)
+		}
 	}
 }
 
@@ -61,6 +84,8 @@ func (c *Client) writeMessages() {
 		c.manager.removeClient(c)
 	}()
 
+	ticker := time.NewTicker(pingInterval)
+
 	for {
 		select {
 		case message, ok := <-c.egress:
@@ -71,10 +96,31 @@ func (c *Client) writeMessages() {
 				return
 			}
 
-			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+			data, err := json.Marshal(message)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
 				log.Printf("failed to send message: %v", err)
 			}
 			log.Println("message sent")
+		// This case is for when we receive a tick from the ticker.
+		case <-ticker.C:
+			log.Println("ping")
+
+			// Send a Ping to the Client
+			if err := c.connection.WriteMessage(websocket.PingMessage, []byte(``)); err != nil {
+				log.Println("writemsg err: ", err)
+				return
+			}
 		}
 	}
+}
+
+func (c *Client) pongHandler(pongMsg string) error {
+	log.Println("pong")
+	// We have to reset the timeline so as to enable subsequent pings and pongs
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
 }
